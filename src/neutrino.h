6eb157b21638e0ce655c8e4fd310b69d31d3b658
FFTEAM-Thomas <fam.trichter@googlemail.com>
2014-10-14 04:51:09 +0200

    luaserver: reorganize (reverted from commit 6de147f0a2d42c2f91131fec2753fb89db8f380c)

--- src/neutrino.h
+++ src/neutrino.h
@@ -36,6 +36,7 @@
 #include "config.h"
 
 #include <configfile.h>
+#include <semaphore.h>
 
 #include <neutrinoMessages.h>
 #include "driver/framebuffer.h"
@@ -177,6 +178,9 @@ public:
 	bool				shairplay_enabled_cur;
 #endif
 	bool                            timer_wakeup;
+
+	sem_t				lua_may_run;
+	sem_t				lua_did_run;
 
 	static CNeutrinoApp* getInstance();
 
