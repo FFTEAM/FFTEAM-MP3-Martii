f37db7d9bdcba35937808d4afe568e28cc857da1
FFTEAM-Thomas <fam.trichter@googlemail.com>
2015-04-09 03:47:24 +0200

    clean

--- src/nhttpd/web/scripts/_Y_Globals.sh
+++ src/nhttpd/web/scripts/_Y_Globals.sh
@@ -18,7 +18,7 @@ boxhardware="spark"
 # -----------------------------------------------------------
 # Pathes
 # -----------------------------------------------------------
-#y_path_httpd="/share/tuxbox/neutrino/httpd"
+y_path_httpd="/share/tuxbox/neutrino/httpd"
 y_path_httpd=".."
 y_path_scripts="$y_path_httpd/scripts"
 y_path_usrbin="/var/bin"
