78a58eca9d36dab4b3ca40027bafa4238a209fba
FFTEAM-Thomas <fam.trichter@googlemail.com>
2015-05-04 23:15:25 +0200

    if zu ifdef

--- src/driver/newclock.h
+++ src/driver/newclock.h
@@ -23,9 +23,10 @@
         Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#if HAVE_SPARK_HARDWARE
+#ifdef HAVE_SPARK_HARDWARE
 #define HAVE_GENERIC_HARDWARE 1
 #endif
+
 #include <lcddisplay/lcddisplay.h>
 
 void InitNewClock();
